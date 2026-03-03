dd60faa5a8ea33b1af7de97e370730d3fd6b6d84
Haruto-works333 <haruto.works333@gmail.com>
2026-03-03 22:12:35 +0900

    init: ソリューション構成作成（UI/Batch/Core/Data 4プロジェクト）

---
+++ ForeignFundManager.Data/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般的な情報は、次の方法で制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更します。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("ForeignFundManager.Data")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("ForeignFundManager.Data")>
+<Assembly: AssemblyCopyright("Copyright © HP 2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("b7c8f3ac-6573-4938-9a8d-af04f31743b1")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      リビジョン
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
